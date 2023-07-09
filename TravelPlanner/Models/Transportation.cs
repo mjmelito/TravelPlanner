1e3b39594fdfd5aa2bed2c279894ba90baacd8a1
Matt Melito <matt_melito@outlook.com>
2023-07-09 14:15:23 -0700

    Remove unnecessary using directives from Transportation.cs.

--- TravelPlanner/Models/Transportation.cs
+++ TravelPlanner/Models/Transportation.cs
@@ -1,12 +1,5 @@
 
 using System.Collections.Generic;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using RestSharp;
-using System.Threading.Tasks;
-using System.Collections.Generic;
-
-using System.ComponentModel.DataAnnotations;
 
 namespace TravelPlanner.Models
 {
