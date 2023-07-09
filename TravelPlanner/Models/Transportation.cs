eae581b3f3213d6dc65520110aca24fb30cd0c7e
Matt Melito <matt_melito@outlook.com>
2023-07-09 10:32:31 -0700

    Add Transportation class with CRUD functionality.

---
+++ TravelPlanner/Models/Transportation.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace TravelPlanner.Models
+{
+  public class Transportation
+  {
+    public int TransportationId { get; set; }
+
+    // [Required(ErrorMessage = "Please enter transportation name.")]
+    public string TransportationName { get; set; }
+
+    public string Mode { get; set; }
+    public string Company { get; set; }
+    public string DepartureLocation { get; set; }
+    public string ArrivalLocation { get; set; }
+
+    public List<TransportationTrip> TransportationTrips { get; set; }
+  }
+}
--- TravelPlanner/Models/TravelPlannerContext.cs
+++ TravelPlanner/Models/TravelPlannerContext.cs
@@ -7,10 +7,11 @@ namespace TravelPlanner.Models
     {
     public DbSet<Trip> Trips { get; set;}
     public DbSet<Destination> Destinations { get; set; }
-    public DbSet<Flight> Flights { get; set; }
+    public DbSet<Transportation> Transportations { get; set; }
     public DbSet<Lodging> Lodgings { get; set; }
     public DbSet<Itinerary> Itineraries { get; set; }
     public DbSet<DestinationTrip> DestinationTrips { get; set; }
+    public DbSet<TransportationTrip> TransportationTrips { get; set; }
     public DbSet<LodgingTrip> LodgingTrips { get; set; }
     public TravelPlannerContext(DbContextOptions options) : base(options) { }
     }
